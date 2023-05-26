2b0678c30b100c76b8beecd0f2629dae1f736b79
razvan-mrc <87325253+razvan-mrc@users.noreply.github.com>
2023-05-26 11:38:35 +0300

    feat: + cron jobs & jszip convert

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from 'express';
 
+import axios from 'axios';
+import JSZip from 'jszip';
+
 import associationController from '@/controllers/association.controller';
 import authController from '@/controllers/auth.controller';
 import batchController from '@/controllers/batch.controller';
@@ -15,10 +18,25 @@ const router = Router();
 router.get(
     '/',
     asyncMiddleware(async (_req: Request, res: Response) => {
-        return res.status(200).json({
-            success: true,
-            message: APP_CONSTANTS.RESPONSE.ROOT.SUCCESS,
-        });
+        const response = await axios.get(
+            'http://localhost:8383/v1/projects/1/forms/Aurora-A-Productor/submissions.csv.zip?attachments=false&groupPaths=true&deletedFields=false&splitSelectMultiples=false'
+        );
+
+        const zip = new JSZip();
+
+        zip.loadAsync(response.data)
+            .then((data) => {
+                console.log(data);
+
+                return res.status(200).json({
+                    success: true,
+                    message: APP_CONSTANTS.RESPONSE.ROOT.SUCCESS,
+                });
+            })
+            .catch((err) => {
+                console.error('Loading error');
+                throw err;
+            });
     })
 );
 
